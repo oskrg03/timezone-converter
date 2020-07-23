22e307c957c5d8a1e0fb534965b89df00b21be0a
Oscar Gamarra <ogamarrao@unicartagena.edu.co>
2020-07-23 14:43:26 -0500

    update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import path from 'path';
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/timezone-converter'));
+app.use(static(__dirname+'/dist/timezone-converter'));
 app.get('/',function(req,res){
     res.sendFile('/timezone-converter/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 8080);
