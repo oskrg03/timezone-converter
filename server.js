c761c3ba7a3afb13157e35fddf349394bfbaa28a
Oscar Gamarra <ogamarrao@unicartagena.edu.co>
2020-07-23 14:39:26 -0500

    update

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/timezone-converter/dist'));
+app.use(express.static(__dirname+'/dist/timezone-converter'));
 app.get('/',function(req,res){
-    res.sendFile('./timezone-converter/dist/index.html');
+    res.sendFile('/timezone-converter/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
